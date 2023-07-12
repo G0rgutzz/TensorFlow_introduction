c7b184a09b06062f6f3f26dace64dd9963695295
G0rgutzz <grzegorz.pyclik1@gmail.com>
2023-07-12 18:48:50 +0200

    Image Data Generation file, almost completed. Requires a way to get around google.colab, as it's not working on pycharm

---
+++ image Data Generation.py
@@ -0,0 +1,101 @@
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import RMSprop
+import numpy as np
+import scipy as misc
+from keras.preprocessing import image
+import zipfile
+import os
+from keras.utils import load_img, img_to_array
+
+
+# Unzip the dataset
+local_zip = './horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('./horse-or-human')
+zip_ref.close()
+
+# Directory with our training horse pictures
+train_horse_dir = os.path.join('./horse-or-human/horses')
+
+# Directory with our training human pictures
+train_human_dir = os.path.join('./horse-or-human/humans')
+# Now see what the filenames look like in the horses and humans training directories:
+train_horse_names = os.listdir(train_horse_dir)
+print(train_horse_names[:10])
+
+train_human_names = os.listdir(train_human_dir)
+print(train_human_names[:10])
+# You can also find out the total number of horse and human images in the directories:
+print('total number of human images: ', len(os.listdir(train_human_dir)))
+print('total number of horse images: ', len(os.listdir(train_horse_dir)))
+"""
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(300, 300),
+    batch_size=32,
+    class_mode='binary')
+    """
+# creating model for the distinction between humans and horses
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(300, 300, 3)),
+    # shape is resizing images to 300x300 and 3 colors as in RGB - 3 bytes per pixel
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+# Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
+    tf.keras.layers.Dense(1, activation='sigmoid')])
+# sigmoid is a bit more efficient in binary
+# 3x convolution and maxpooling reflects higher complexity and size of the images
+model.summary()
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(learning_rate=0.001),  # learning rate, it is used to adjust the performance
+              metrics=['accuracy'])
+train_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(  # loads images from the directory and it's subdirectories
+    './horse-or-human/',  # This is the source directory for training images
+    target_size=(300, 300),  # All images will be resized to 300x300
+    batch_size=128,
+    class_mode='binary')  # Since we use binary_crossentropy loss, we need binary labels
+history = model.fit(
+      train_generator,
+      steps_per_epoch=8,
+      epochs=15,
+      verbose=1)
+
+# model prediction
+# need to find a way to use this on pycharm, as jupyter only allows it
+"""
+uploaded = files.upload()
+for fn in uploaded.keys():
+    # predicting images
+    path = '/content/' + fn
+    img = image.load_img(path, target_size=(300, 300))
+    x = image.img_to_array(img)
+    y = np.expand_dims(x, axis=0)
+
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
+    print(classes[0])
+    if classes[0] > 0.5:
+        print(fn + "is a human")
+    else:
+        print(fn + "is a horse")
+"""
